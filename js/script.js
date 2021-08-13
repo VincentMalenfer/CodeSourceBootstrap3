2c6e5a8221277b68e4d8ec6fe6b78c736573256c
VincentMalenfer <vincent.malenfer@outlook.fr>
2021-08-13 11:43:14 +0200

    Ajout du code PHP pour le formulaire de contact.

--- js/script.js
+++ js/script.js
@@ -9,4 +9,31 @@ $(function(){
         
     });
 
+    $('#contact-form').submit(function (e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function (json) {
+
+                if (json.isSuccess) {
+                    $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté !😊</p>");
+                    $('#contact-form')[0].reset();
+                }
+                else {
+                    $('#firstname + .comments').html(json.firstnameError);
+                    $('#name + .comments').html(json.nameError);
+                    $('#email + .comments').html(json.emailError);
+                    $('#phone + .comments').html(json.phoneError);
+                    $('#message + .comments').html(json.messageError);
+                }
+            }
+        });
+    });
+
 })
